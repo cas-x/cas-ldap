1308fa6a671d0dcaad31ef78551a455d03f54a0d
detailyang <detailyang@gmail.com>
2016-03-30 11:18:59 +0800

    update monitor

--- monitor.js
+++ monitor.js
@@ -3,7 +3,7 @@
 * @Date:   2016-03-18T12:08:27+08:00
 * @Email:  detailyang@gmail.com
 * @Last modified by:   detailyang
-* @Last modified time: 2016-03-18T13:15:35+08:00
+* @Last modified time: 2016-03-30T11:18:56+08:00
 * @License: The MIT License (MIT)
 */
 
@@ -16,7 +16,7 @@ const salt = process.env.CAS_NOTP_SALT || '$2a$10$jsZ0onecMnHOeKUfRG9AYe';
 config.mock = {
   username: 'monitor',
   password: 'monitor_MM',
-  id: '466',
+  id: '482',
   gender: 0,
   realname: 'monitor',
   aliasname: 'monitor',
@@ -24,7 +24,7 @@ config.mock = {
   email: 'monitor@example.com',
   key: '',
   dynamic: (()=>{
-    const key = password.decodeGoogleSecret('EQZGCJBRGASGU422GBXW4ZLDJVXEQT3FJNKWMUSHHFAVSZLWIRWTQWKVKBVHS3DYOY3GM5LBKEZTKNDPORFTASRVOFTVC2LN');
+    const key = password.decodeGoogleSecret('EQZGCJBRGASGU422GBXW4ZLDJVXEQT3FJNKWMUSHHFAVSZLJI5MGYY3OOFWEIQLBI5STQV3SPFVXA3JXOQZHA6LBJZIGMQLR');
     const otpcode = password.otpgen(key);
 
     return otpcode;
